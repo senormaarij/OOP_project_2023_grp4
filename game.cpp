8e4dcdac387bcdf1db6ffd642a5e76c0bdf27d5a
sherali9699 <sheralikhan9699@gmail.com>
2023-12-02 14:36:20 +0500

    I have just added start screen that would show the game screen on the press of enter

--- game.cpp
+++ game.cpp
@@ -3,34 +3,39 @@
 #include "GameObj.hpp"
 #include <vector>
 
-Mix_Music *music=nullptr;
+Mix_Music *music = nullptr;
 Player* player;
 GameObject *obj1;
 GameObject *obj2;
 std::vector<GameObject*> gameObjects;
 std::vector<GameObject*> map;
-int i =0;
+int i = 0;
 SDL_Renderer* Game::Renderer = nullptr;
 
+enum GameState {
+    START_SCREEN,
+    GAME_SCREEN,
+    // Add more states as needed
+};
+
+GameState currentState = START_SCREEN;
 
 void Game::init(const char* title, int x, int y)
 {
-	if(SDL_Init(SDL_INIT_EVERYTHING)== 0){
-		std::cout<< "Init successful\n";
-	}
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        std::cout << "Init successful\n";
+    }
 
-	Window = SDL_CreateWindow(title,x,y,SCREEN_WIDTH,SCREEN_HEIGHT,0);
-	
+    Window = SDL_CreateWindow(title, x, y, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
 
-	Renderer = SDL_CreateRenderer(Window,-1,SDL_RENDERER_ACCELERATED);
-
-	SDL_SetRenderDrawColor(Renderer,255,255,255,255);
-
-	// Initialize SDL_mixer for audio
+    // Initialize SDL_mixer for audio
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
         std::cout << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
     }
-	// Load the audio file
+
+    // Load the audio file
     music = Mix_LoadMUS("assets/Hitman(chosic.com).mp3");
 
     if (music == nullptr) {
@@ -39,48 +44,64 @@ void Game::init(const char* title, int x, int y)
         Mix_PlayMusic(music, -1); // Play the loaded music infinitely
     }
 
+    player = new Player("assets/fire..png");
+    obj1 = new Objects("assets/Sprite-0001.jpg", 600, 200);
+    obj2 = new Objects("assets/platform.png", 400, 100);
 
-	player = new Player("assets/fire..png");
-	obj1 = new Objects("assets/Sprite-0001.jpg", 600, 200);
-	obj2 = new Objects("assets/platform.png", 400, 100);
-	
-	gameObjects.push_back(obj1);
-	gameObjects.push_back(obj2);
-
-	
+    gameObjects.push_back(obj1);
+    gameObjects.push_back(obj2);
 }
 
-bool Game::isRunning(){
-	return run;
+bool Game::isRunning()
+{
+    return run;
 }
 
-void Game::render(){
-	SDL_RenderClear(Renderer);
-	player->Render();
-	obj1->Render();
-	obj2->Render();
-	SDL_RenderPresent(Renderer);
+void Game::render()
+{
+    SDL_RenderClear(Renderer);
+
+    // Render based on the current state
+    if (currentState == START_SCREEN) {
+		SDL_Texture* startScreen = loadtexture::LoadTexture("assets/gameStart.webp");
+		SDL_RenderCopy(Renderer, startScreen, NULL, NULL);
+        // Render starting screen elements here
+    } else if (currentState == GAME_SCREEN) {
+        player->Render();
+        obj1->Render();
+        obj2->Render();
+    }
+
+    SDL_RenderPresent(Renderer);
 }
 
-void Game::update(){
-	player->Update(gameObjects);
+void Game::update()
+{
+    // Update based on the current state
+    if (currentState == GAME_SCREEN) {
+        player->Update(gameObjects);
+        obj1->Update();
+        obj2->Update();
+    }
 
-	obj1->Update();
-	obj2->Update();
-	std::cout << """""""""" << std::endl;
-	//std::cout <<player->returnmove() << std::endl;
-
+    std::cout << """""""""" << std::endl;
 }
 
-void Game::events(){
-	SDL_Event event;
-	SDL_PollEvent(&event);
-	switch(event.type){
-		case SDL_QUIT:
-			run = false;
-			break;
-		case SDL_KEYUP:
+void Game::events()
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT:
+            run = false;
+            break;
+        case SDL_KEYUP:
             switch (event.key.keysym.sym) {
+                case SDLK_RETURN:
+                    // Transition to the game screen on pressing Enter
+                    currentState = GAME_SCREEN;
+                    break;
                 case SDLK_LEFT:
                 case SDLK_RIGHT:
                     player->Stop();
@@ -89,27 +110,24 @@ void Game::events(){
             break;
     }
 
+    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 
-	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-    if (currentKeyStates[SDL_SCANCODE_UP]) {
-        player->Jump();
+    if (currentState == GAME_SCREEN) {
+        if (currentKeyStates[SDL_SCANCODE_UP]) {
+            player->Jump();
+        } else if (currentKeyStates[SDL_SCANCODE_LEFT]) {
+            player->MoveLeft();
+        } else if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
+            player->MoveRight();
+        }
     }
-    else if (currentKeyStates[SDL_SCANCODE_LEFT]) {
-        player->MoveLeft();
-    }
-    else if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
-        player->MoveRight();
-	}
 }
 
 void Game::close()
 {
-	SDL_DestroyRenderer( Renderer );
-	SDL_DestroyWindow( Window );
-	Window = NULL;
-	Renderer = NULL;
-	SDL_Quit();
+    SDL_DestroyRenderer(Renderer);
+    SDL_DestroyWindow(Window);
+    Window = NULL;
+    Renderer = NULL;
+    SDL_Quit();
 }
-
-
