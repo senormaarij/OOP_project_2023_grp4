cbfb7250fd1a14625b39be49711b9c5e41a33bd1
senormaarij <122529580+senormaarij@users.noreply.github.com>
2023-12-05 00:21:35 +0500

    Rework changes

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+class Vector2D{
+public:
+    float x, y;
+    Vector2D(){x = 0; y = 0;}
+
+	Vector2D(float p_x, float p_y):x(p_x), y(p_y){}
+
+	void print(){
+		std::cout << x << ", " << y << std::endl;
+	}
+};
